3f26832c2743ffd37239a2171adffa7c32fe4bd1
almirph <phelipear@gmail.com>
2022-07-02 16:28:32 -0300

    codigo atualizado

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,15 @@ import System.Random (randomRIO)
 
 main :: IO ()
 main = do  
-    numList <- randomList(4)
-    putStrLn "?"  
-    name <- getLine  
-    putStrLn ("Hey " ++ name ++ ", you rock!")
-    putStrLn(show numList)
+    keyList <- randomList(4)
+    putStrLn "?" 
+    guesString <- getLine
+    let guesList = map read $ words guesString :: [Int]
+    hits <- compareGuesAndRandom guesList guesList 0 (0, 0)
+    putStrLn(show keyList)
+    putStrLn(show guesList)
+    print(hits)
+    print(keyList!!0)
 
 randomList :: Int -> IO([Int])
 randomList 0 = return []
@@ -16,3 +20,19 @@ randomList n = do
     r  <- randomRIO (1,6)
     rs <- randomList (n-1)
     return (r:rs) 
+
+compareGuesAndRandom :: [Int] -> [Int] -> Int -> (Int, Int) -> IO(Int, Int)
+compareGuesAndRandom guesList keyList 4 hits = do
+    return hits
+compareGuesAndRandom guesList keyList position hits = do
+    let guesElement = guesList!!position
+    let keyElement = keyList!!position
+    if guesElement == keyElement
+    then do
+        print("igual")
+        newHits <- compareGuesAndRandom guesList keyList 4 hits
+        return newHits
+    else do
+        newHits <- compareGuesAndRandom guesList keyList 4 hits
+        return newHits
+
