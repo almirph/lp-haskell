ba5274db1a3dcfb4fe033d19d30414c58d533110
almirph <phelipear@gmail.com>
2022-07-02 18:00:25 -0300

    verificações de lista de parciais

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,10 @@ main = do
     putStrLn "?" 
     guesString <- getLine
     let guesList = map read $ words guesString :: [Int]
-    hits <- compareGuesAndRandom guesList keyList 0 (0, 0)
+    hits <- compareGuesAndRandom guesList keyList [] 0 (0, 0)
     putStrLn(show keyList)
     putStrLn(show guesList)
     print(hits)
-    print(keyList!!0)
 
 randomList :: Int -> IO([Int])
 randomList 0 = return []
@@ -21,17 +20,46 @@ randomList n = do
     rs <- randomList (n-1)
     return (r:rs) 
 
-compareGuesAndRandom :: [Int] -> [Int] -> Int -> (Int, Int) -> IO(Int, Int)
-compareGuesAndRandom guesList keyList 4 hits = do
+compareGuesAndRandom :: [Int] -> [Int] -> [Int] -> Int -> (Int, Int) -> IO(Int, Int)
+compareGuesAndRandom guesList keyList partials 4 hits = do
     return hits
-compareGuesAndRandom guesList keyList position hits = do
+compareGuesAndRandom guesList keyList partials position hits = do
     let newPosition = position + 1
     if guesList!!position == keyList!!position
     then do
-        print("igual")
-        newHits <- compareGuesAndRandom guesList keyList newPosition hits
+        let (complete, _) = hits
+        let (_, partial) = hits
+        let hitsAux = (complete + 1, partial)
+        newHits <- compareGuesAndRandom guesList keyList partials newPosition hitsAux
         return newHits
     else do
-        newHits <- compareGuesAndRandom guesList keyList newPosition hits
-        return newHits
+        let guesListPosition = guesList!!position
+        let existsImPartial = isMember guesListPosition partials
+        hadPartial <- comparePartial keyList 0 guesListPosition
+        if hadPartial && not existsImPartial
+        then do
+            let (complete, _) = hits
+            let (_, partial) = hits
+            let hitsAux = (complete, partial + 1)
+            let newPartial = partials ++ [guesListPosition]
+            newHits <- compareGuesAndRandom guesList keyList newPartial newPosition hitsAux
+            return newHits
+        else do
+            newHits <- compareGuesAndRandom guesList keyList partials newPosition hits
+            return newHits
 
+comparePartial :: [Int] -> Int -> Int -> IO(Bool)
+comparePartial keyList 4 compareInt = do
+    return False
+comparePartial keyList position compareInt = do
+    if compareInt == keyList!!position
+    then return True
+    else do
+        let newPosition = position + 1
+        newComparePartial <- comparePartial keyList newPosition compareInt
+        return newComparePartial
+
+isMember n [] = False
+isMember n (x:xs)
+    | n == x = True
+    | otherwise = isMember n xs
